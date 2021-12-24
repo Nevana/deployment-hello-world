4dee4acf8290451d6ce6349963688d7bd9c3aecc
Tim Harpe <tim.harpe@t-online.de>
2021-12-25 00:44:45 +0100

    feat: initial commit

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "echo" {
+  value = null_resource.echo
+}
+
+output "message" {
+  value = var.message
+}
---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~>1.0.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
